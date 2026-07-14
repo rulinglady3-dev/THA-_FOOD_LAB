22b368423442f75a6304364b27fe7f22d80aa0ec
rulinglady3-dev <rulinglady3@gmail.com>
2026-07-14 21:21:44 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+const startButton = document.getElementById("start");
+const result = document.getElementById("result");
+const food = document.querySelector(".food");
+
+
+startButton.addEventListener("click",()=>{
+
+
+    startButton.disabled = true;
+
+    startButton.innerHTML = "ANALYZING...";
+
+    result.innerHTML =
+    "🔬 Scanning ingredients...";
+
+
+    food.style.animation =
+    "pulse 0.5s infinite";
+
+
+    setTimeout(()=>{
+
+        result.innerHTML =
+        "🌶 Detecting spices...";
+
+    },2000);
+
+
+
+    setTimeout(()=>{
+
+        result.innerHTML =
+        "🥥 Checking flavor balance...";
+
+    },4000);
+
+
+
+    setTimeout(()=>{
+
+        result.innerHTML =
+        "✅ Experiment completed!<br><br>Thai flavor profile found 🍜";
+
+        startButton.innerHTML =
+        "NEW EXPERIMENT";
+
+        startButton.disabled=false;
+
+
+    },6000);
+
+
+
+});
